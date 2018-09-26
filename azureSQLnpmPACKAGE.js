155690661b05223d1e8e1b4987553a4b4f7db9a3
Squiff88 <metodieff.stefan@gmail.com>
2018-09-26 13:02:22 +0200

    sql connection

---
+++ azureSQLnpmPACKAGE.js
@@ -0,0 +1,13 @@
+// https://www.npmjs.com/package/mssql
+
+const sql = require('mssql')
+ 
+async () => {
+    try {
+        await sql.connect('mssql://username:password@localhost/database')
+        const result = await sql.query`select * from mytable where id = ${value}`
+        console.dir(result)
+    } catch (err) {
+        // ... error checks
+    }
+}
